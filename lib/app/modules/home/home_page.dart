4d51e7436a5a8333c790d5fece0ccafb6cfcd1b4
yasin <yasinmiapolash@gmail.com>
2024-05-23 13:01:06 +0600

    first commit

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home_controller.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomePage is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
